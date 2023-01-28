d774416dd5b0b5e7217975e39d1fdd027d7e60b8
Вячеслав Беляков <zxc7613@mail.ru>
2023-01-29 04:49:08 +0700

    Поддержка свагера

--- src/Console/Commands/MakeDoc.php
+++ src/Console/Commands/MakeDoc.php
@@ -28,6 +28,52 @@ class MakeDoc extends Command
     protected $description = 'Создать php doc описание к модели';
 
 
+    public static function IssetShema($f)
+    {
+        return strpos($f, "* @OA\Schema(") > 0;
+    }
+
+    public static function GetExistPrpoperys($f, $proplist, $nameClass)
+    {
+
+        $existProps = [];
+
+        $getAllProperties = explode('@property', $f);
+        foreach ($getAllProperties as $K => $V) if ($K > 0) {
+            $line = explode("\n", $V)[0];
+            $line = '@property' . $line;
+            foreach ($proplist as $K => $prop) {
+                if (strpos($line, $K) > 0) {
+                    $existProps[$K] = true;
+                }
+            }
+            if (strpos($V, "class " . $nameClass) > -1) break;
+        }
+        return $existProps;
+    }
+
+
+    public static function GetExistSchemePrpoperties($f, $proplist, $nameClass)
+    {
+
+        $existProps = [];
+
+        $getAllProperties = explode('@OA\Property', $f);
+        foreach ($getAllProperties as $K => $V) if ($K > 0) {
+            $line = explode("),", $V)[0];
+            $line = str_replace("\n", "", $line);
+            $line = str_replace(" ", "", $line);
+            foreach ($proplist as $K => $prop) {
+                if (strpos($line, 'property="' . $K . '"') > 0) {
+                    $existProps[$K] = true;
+                }
+            }
+            if (strpos($V, "class " . $nameClass) > -1) break;
+        }
+        return $existProps;
+    }
+
+
     public function handle()
     {
         $name = $this->argument("Model");
@@ -48,42 +94,64 @@ class MakeDoc extends Command
         $needClassStart = strtolower("class" . $name . 'extends');
 
         $proplist = $model->GetProperties();
+
+        $existProps = self::GetExistPrpoperys($f, $proplist, $name);
+        $existPropsSheme = self::GetExistSchemePrpoperties($f, $proplist, $name);
+
+
+        $docPropertyBlock = "/**\n";
+        foreach ($proplist as $K => $prop) {
+            if (isset($existProps[$K])) continue;
+            $docPropertyBlock .= "\n * @property " . MigrationRender::GetType($prop->typeData);
+            if (!$prop->default) $docPropertyBlock .= "|null ";
+            $docPropertyBlock .= '$' . $K . ' ' . ($prop->comment ?? $prop->label ?? $prop->descr ?? " ");
+        }
+        $docPropertyBlock .= "\n*/";
+
+
+        $docScheme = "";
+        foreach ($proplist as $K => $prop) {
+            if (isset($existPropsSheme[$K])) continue;
+            $docScheme .= "\n " . '@OA\Property( property="' . $K . '", ';
+            $docScheme .= "\n ".' description="' . ($prop->comment ?? $prop->label ?? $prop->descr ?? "Без описания") . '"),';
+
+        }
+        $docScheme = trim($docScheme,'');
+       // $docScheme = trim($docScheme,',');
+        $docScheme = str_replace("\n", "\n *     ", $docScheme);
+
         foreach (explode("\n", $f) as $line) {
+
+
+
             if ($needClassStart) {
 
-                //ищем все парметры здесь
-                if (substr_count($line, '@property')) {
-                    foreach ($proplist as $K => $V) {
-                        if (substr_count($line, "$" . $K." ")) {
-                            unset($proplist[$K]);
-                            break;
-                        }
-                    }
-                }
-
                 $s = str_replace(" ", "", $line);
-
                 $s = strtolower($s);
                 $s = trim($s);
+
+
+                if (strpos($line, "@OA\Schema(") > 0) {
+                    $fout .= $line  ;
+                    $line="";
+                    $fout .=  $docScheme  ;
+                }
+
+
                 if (strpos($s, $needClassStart) === 0) {
-
-                    /** * @var  PropertyBuilderStructure $prop */
-                    foreach ($proplist as $K => $prop) {
-                        $fout .= "\n /** @property " . MigrationRender::GetType($prop->typeData);
-                        if (!$prop->default) $fout .= "|null ";
-                        $fout .= '$' . $K . ' ' . ($prop->comment ?? $prop->label ?? $prop->descr ?? " ");
-                        $fout .= ' */';
-                        $this->info("add @property ".$K);
-                    }
-
+                    $fout .= "\n" . $docPropertyBlock . "\n";
                     $needClassStart = null;
                 }
+
+
             }
 
-            $fout .= "\n" . $line;
+            $fout .= $line . "\n";
         }
 
         $fout = trim($fout);
+        $fout = str_replace("*/\n/**\n", "", $fout);
+        $fout = str_replace("*/\n\n/**\n", "", $fout);
 
 
         file_put_contents($pTo, $fout);
